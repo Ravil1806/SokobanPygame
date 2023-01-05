dc36aca49a45576831b1d5ce4454934b8f17c9ea
ravil1806 <ravil1806@mail.ru>
2023-01-05 16:31:19 +0600

    Third commit

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import sys
 
 FPS = 50
 
-# all_sprites = pygame.sprite.Group()
+player = None
+
+# 3 группы спрайтов
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
 
 # Загрузка изображений
@@ -14,24 +19,77 @@ def load_image(name):
     return image
 
 
-all_sprites = pygame.sprite.Group()
-sprite = pygame.sprite.Sprite()
-sprite.image = load_image("bottle.png")
-sprite.rect = sprite.image.get_rect()
-all_sprites.add(sprite)
+tile_images = {
+    'wall': load_image('brickwall.png'),
+    'empty': load_image('floor.png'),
+    'object': load_image('bottle.png'),
+    'place': load_image('place.png')
+}
+player_image = load_image('builder.png')
+
+tile_width = tile_height = 50
+
+
+def load_level(filename):
+    filename = "data/levels/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+            elif level[y][x] == '$':
+                Tile('object', x, y)
+            elif level[y][x] == '&':
+                Tile('place', x, y)
+    return new_player, x, y
 
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Кладовщик')
-    size = width, height = 800, 600
+    size = width, height = 1000, 750
     screen = pygame.display.set_mode(size)
     screen.fill('white')
+    mapp = load_level('level1.txt')
+    player, level_x, level_y = generate_level(mapp)
     all_sprites.draw(screen)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                terminate()
         pygame.display.flip()
     pygame.quit()
